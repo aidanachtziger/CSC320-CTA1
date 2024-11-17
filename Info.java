843a7d958130ceca0fc67324e8634c7b09eabfa0
aidanachtziger <aidan.achtziger@gmail.com>
2024-11-16 17:43:40 -0700

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,16 @@
+public class Info {
+    public static void main(String[] args) {
+        String first = "John";
+        String last = "Doe";
+        String address = "1234 Street Road";
+        String city = "Denver";
+        String zip = "12345";
+
+        System.out.println("First name: " + first);
+        System.out.println("Last name: " + last);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("Zip code: " + zip);
+    }
+    
+}
